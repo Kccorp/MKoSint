a550c245cff657362baf4a88269c31154081c7ef
Kccorp <keissy.bod@hotmail.com>
2022-11-25 14:33:12 +0100

    Installation setup

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Ready for new project")
+import os
+import sys
+import time
+import pip
+
+
+def main():
+    installation()
+
+
+def installation():
+    if not os.path.exists("dnscan"):
+        os.system("git clone https://github.com/rbsec/dnscan.git")
+
+    # install the dependencies
+    print("Dependency installation...")
+    time.sleep(1)
+    os.system("cd dnscan && pip install -r requirements.txt")
+    print("Installation complete!")
+
+# def userSelection(domain, ):
+
+
+if __name__ == '__main__':
+    main()
